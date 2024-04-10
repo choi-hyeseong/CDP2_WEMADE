8bb4dbc77e83feb4425ac411e21f4da50446f4b4
choi-hyeseong <bd2845@kakao.com>
2024-04-10 12:43:01 +0900

    test: fix chart test

--- APP/app/src/test/java/com/home/cdp2app/view/chart/mapper/HeartRateChartMapperTest.kt
+++ APP/app/src/test/java/com/home/cdp2app/view/chart/mapper/HeartRateChartMapperTest.kt
@@ -1,6 +1,7 @@
 package com.home.cdp2app.view.chart.mapper
 
 import com.home.cdp2app.health.heart.entity.HeartRate
+import com.home.cdp2app.view.chart.Chart
 import com.home.cdp2app.view.chart.ChartItem
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -18,21 +19,21 @@ class HeartRateChartMapperTest {
         val firstRate = HeartRate(Instant.now().minusSeconds(1), 150)
         val secondRate = HeartRate(Instant.now().minusSeconds(2), 140)
         val heartRates = mutableListOf(firstRate, secondRate)
-        val items : List<ChartItem> = mapper.convertToChart(heartRates)
-        assertEquals(2, items.size)
+        val chart : Chart = mapper.convertToChart(heartRates)
+        assertEquals(2, chart.chartData.size)
         //chart data는 double로 형변환 됨
-        assertEquals(firstRate.time, items[0].time)
-        assertEquals(firstRate.bpm.toDouble(), items[0].data, 0.0)
-        assertEquals(secondRate.time, items[1].time)
-        assertEquals(secondRate.bpm.toDouble(), items[1].data, 0.0)
+        assertEquals(firstRate.time, chart.chartData[0].time)
+        assertEquals(firstRate.bpm.toDouble(), chart.chartData[0].data, 0.0)
+        assertEquals(secondRate.time, chart.chartData[1].time)
+        assertEquals(secondRate.bpm.toDouble(), chart.chartData[1].data, 0.0)
     }
 
     @DisplayName("리스트가 빈경우 빈 차트 아이템 반환")
     @Test
     fun TEST_EMPTY_MAP_TO_CHART() {
         val heartRates = mutableListOf<HeartRate>()
-        val items : List<ChartItem> = assertDoesNotThrow { mapper.convertToChart(heartRates) }
-        assertEquals(0, items.size)
+        val chart : Chart = assertDoesNotThrow { mapper.convertToChart(heartRates) }
+        assertEquals(0, chart.chartData.size)
 
     }
 }
