b2406197513c68a7d5e85d416e674c4b546e2bfb
choi-hyeseong <bd2845@kakao.com>
2024-04-12 16:31:52 +0900

    comment: add todo in SleepHourChartMapper

--- APP/app/src/main/java/com/home/cdp2app/view/chart/mapper/SleepHourChartMapper.kt
+++ APP/app/src/main/java/com/home/cdp2app/view/chart/mapper/SleepHourChartMapper.kt
@@ -8,6 +8,7 @@ import com.home.cdp2app.view.chart.type.HealthCategory
 //수면시간 - 차트 매핑 클래스
 class SleepHourChartMapper : ChartMapper<SleepHour>() {
     //수면시간을 시간단위로 변경해 Chart로 생성함.
+    //TODO 시간단위를 좀더 계산해 1.5시간, 0.5시간으로 변경하기 (ex) 90분 수면 -> 1.5시간)
     override fun convertToChart(entities: List<SleepHour>): Chart {
         return Chart(HealthCategory.SLEEP_HOUR, entities.map { ChartItem(it.date, it.duration.toHours().toDouble()) })
     }
