9ec93855398e03cbea87e77fbd66ec32b35ed63d
choi-hyeseong <bd2845@kakao.com>
2024-05-03 14:34:52 +0900

    feat: create BasicInfo view - init

---
+++ APP/app/src/main/java/com/home/cdp2app/view/fragment/BasicInfoFragment.kt
@@ -0,0 +1,8 @@
+package com.home.cdp2app.view.fragment
+
+/**
+ * 기본 건강정보 (BasicInfo)를 수정하고 관리하는 프래그먼트
+ */
+class BasicInfoFragment {
+
+}
---
+++ APP/app/src/main/java/com/home/cdp2app/view/viewmodel/BasicInfoViewModel.kt
@@ -0,0 +1,8 @@
+package com.home.cdp2app.view.viewmodel
+
+/**
+ * BasicInfoFragment에 사용되는 VM
+ */
+class BasicInfoViewModel {
+
+}
