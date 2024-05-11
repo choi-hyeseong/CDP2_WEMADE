8cf202c34ad2fd37fbf296ed3b8be698a198fcb8
choi-hyeseong <bd2845@kakao.com>
2024-05-11 21:21:40 +0900

    comment: add comment for valdiate status

--- APP/app/src/main/java/com/home/cdp2app/type/ValidateStatus.kt
+++ APP/app/src/main/java/com/home/cdp2app/type/ValidateStatus.kt
@@ -2,9 +2,11 @@ package com.home.cdp2app.type
 
 /**
  * 값저장시 validate한 결과를 나타내는 enum 입니다.
+ *
+ * 추후 개선이 가능하다면 ValidateResult(status : ValidateStatus, field : String)의 형태로 개선, 어떤 필드에서 검증이 통과하지 못했는지 확인할 수 있도록..
  * @property OK validate가 성공했습니다.
  * @property FIELD_EMPTY 값이 비어있습니다.
- * @property VALUE_ERROR - 올바른 값이 들어있지 않습니다. (숫자 필드에 문자열..)
+ * @property VALUE_ERROR 올바른 값이 들어있지 않습니다. (숫자 필드에 문자열..)
  */
 enum class ValidateStatus {
     OK, FIELD_EMPTY, VALUE_ERROR
