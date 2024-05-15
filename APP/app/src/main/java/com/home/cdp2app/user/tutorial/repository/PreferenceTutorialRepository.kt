caab9ebbcc1a0e123fb84e0e80f563866d4576ed
choi-hyeseong <bd2845@kakao.com>
2024-05-15 13:35:55 +0900

    feat: create tutorial repository

---
+++ APP/app/src/main/java/com/home/cdp2app/user/tutorial/repository/PreferenceTutorialRepository.kt
@@ -0,0 +1,22 @@
+package com.home.cdp2app.user.tutorial.repository
+
+import com.home.cdp2app.memory.SharedPreferencesStorage
+
+/**
+ * SharedPreference를 사용해서 Tutorial 여부를 저장하는 레포지토리
+ * @property preferencesStorage tutorial 여부를 관리하는 preference 입니다.
+ */
+class PreferenceTutorialRepository(private val preferencesStorage: SharedPreferencesStorage) : TutorialRepository {
+
+    private val KEY : String = "TUTORIAL"
+
+    override suspend fun checkCompleted(): Boolean {
+        return preferencesStorage.getBoolean(KEY, false)
+    }
+
+    override suspend fun setCompleted() {
+        preferencesStorage.putBoolean(KEY, true)
+    }
+
+
+}
