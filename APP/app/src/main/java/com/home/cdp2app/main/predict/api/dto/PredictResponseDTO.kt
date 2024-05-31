c1e99b6668d8e16855d57dda7c4da1d4bcda7a24
choi-hyeseong <bd2845@kakao.com>
2024-05-31 20:05:07 +0900

    fix: fix toEntity method for display percent (0~1)

--- APP/app/src/main/java/com/home/cdp2app/main/predict/api/dto/PredictResponseDTO.kt
+++ APP/app/src/main/java/com/home/cdp2app/main/predict/api/dto/PredictResponseDTO.kt
@@ -4,13 +4,13 @@ import com.home.cdp2app.main.predict.entity.PredictResult
 
 /**
  * Predict 요청의 결과값을 나타냅니다.
- * @property prediction_result 고혈압 확률을 나타냅니다.
+ * @property prediction_result 고혈압 확률을 나타냅니다. 0~1 사이
  */
-class PredictResponseDTO(private val prediction_result : Double) {
+data class PredictResponseDTO(private val prediction_result : Float) {
 
     /**
      * 엔티티로 변환하는 함수
      * @return PredictResult 엔티티로 변환합니다.
      */
-    fun toEntity() : PredictResult = PredictResult(prediction_result)
+    fun toEntity() : PredictResult = PredictResult(prediction_result * 100.0)
 }
