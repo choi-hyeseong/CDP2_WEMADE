7c36750864808e226df5deb33bf92a68aad2ef69
sunchipq <wjsalswo369@naver.com>
2024-06-02 06:55:02 +0900

    feat: cnn 모델 적용

--- BACKEND/src/health/health.service.ts
+++ BACKEND/src/health/health.service.ts
@@ -6,38 +6,53 @@ import * as path from 'path';
 @Injectable()
 export class HealthService {
   constructor(private configService: ConfigService) {}
+
   private model: tf.LayersModel | null = null;
+
+  private fields = [
+    'sex',
+    'age',
+    'HE_sbp',
+    'HE_dbp',
+    'HE_BMI',
+    'total_sleep',
+    'sm_present',
+    'pa_walk',
+  ];
 
   async loadModel() {
     if (!this.model) {
-      const modelPath = path.resolve(__dirname, '../../test_model/model.json');
+      const modelPath = path.resolve(
+        __dirname,
+        '../../predict_model/model.json',
+      );
       this.model = await tf.loadLayersModel(`file://${modelPath}`);
     }
   }
 
   normalize(data: { [key: string]: number }): number[] {
-    const normalizedData = Object.values(data).map((value, index) => {
-      const fieldName = Object.keys(data)[index];
-      const min = this.configService.get<number>(
-        `MIN_${fieldName.toUpperCase()}`,
-      );
-      const max = this.configService.get<number>(
-        `MAX_${fieldName.toUpperCase()}`,
-      );
-      return (value - min) / (max - min);
-    });
+    return this.fields.map((field) => {
+      const value = Math.pow(data[field], 2);
 
-    return normalizedData;
+      const mean = this.configService.get<number>(
+        `MEAN_${field.toUpperCase()}`,
+      );
+      const std = this.configService.get<number>(`STD_${field.toUpperCase()}`);
+      const normalizedValue = (value - mean) / std;
+
+      return normalizedValue;
+    });
   }
 
   async predictHealthInfo(data: { [key: string]: number }) {
     await this.loadModel();
 
-    const inputData = this.normalize(data);
+    const inputData = tf.tensor3d(
+      [this.normalize(data).map((value) => [value])],
+      [1, this.fields.length, 1],
+    );
 
-    const outputData = this.model.predict(
-      tf.tensor2d([inputData], [1, 9]),
-    ) as tf.Tensor;
+    const outputData = this.model.predict(inputData) as tf.Tensor;
     const prediction_result = await outputData.data();
 
     return { prediction_result: prediction_result[0] };
